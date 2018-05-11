e8b7d44d406518c81f709dc506c466d18cda7176
PoLáKoSz Tom <PoLaKoSz@users.noreply.github.com>
2018-05-11 17:25:07 +0200

    Fix #5, fix #6 - Delay working again
    - Variable types must be Integer
    - +/- delay random bug fixed

--- src/js/background.js
+++ src/js/background.js
@@ -2,13 +2,13 @@
   What happens when the extension icon is clicked ...
 */
 chrome.browserAction.onClicked.addListener(function() {
-    var numRefresh = localStorage["numRefresh"];
-    var appendInt = localStorage["append_int"];
-    var appendIntMin = localStorage["append_int_min"];
-    var appendIntMax = localStorage["append_int_max"];
-    var inputURL = localStorage["input_url"];
-    var DELAY = localStorage["delay"];
-    var PLUS_DELAY = localStorage["plus_delay"];
+    var REFRESH_COUNT = parseInt(localStorage["numRefresh"]);
+    var IS_APPEND_INT = localStorage["append_int"];
+    var APPEND_INT_MIN = parseInt(localStorage["append_int_min"]);
+    var APPEND_INT_MAX = parseInt(localStorage["append_int_max"]);
+    var INPUT_URL = localStorage["input_url"];
+    var DELAY = parseInt(localStorage["delay"]);
+    var PLUS_DELAY = parseInt(localStorage["plus_delay"]);
 
     /*
      Get the current tab, tab.id is passed into tab.update so that 
@@ -20,17 +20,19 @@ chrome.browserAction.onClicked.addListener(function() {
         var counter = 0;
 
         (function search() {
-            if (numRefresh == 0 || counter < numRefresh) {
-                if (appendInt === 'true') {
-                    var randomInt = Math.floor((Math.random() * appendIntMax) + appendIntMin);
-                    chrome.tabs.update(tab.id, { 'url': inputURL + randomInt });
+            if (REFRESH_COUNT == 0 || counter < REFRESH_COUNT) {
+                if (IS_APPEND_INT === 'true') {
+                    var randomInt = Math.floor((Math.random() * APPEND_INT_MAX) + APPEND_INT_MIN);
+                    chrome.tabs.update(tab.id, { 'url': INPUT_URL + randomInt });
                 } else {
-                    chrome.tabs.update(tab.id, { 'url': inputURL });
+                    chrome.tabs.update(tab.id, { 'url': INPUT_URL });
                 }
 
                 counter += 1;
 
-                var real_delay = (DELAY + Math.floor((Math.random() * PLUS_DELAY) * 1000));
+                var small_delay = Math.floor(Math.random() * 10) - PLUS_DELAY;
+                var real_delay = (DELAY + small_delay) * 1000;
+
                 setTimeout(search, real_delay);
             }
         })();
