562b44b9e58ab2bcbf5c66d76c899f7634cf5441
PoLáKoSz Tom <PoLaKoSz@users.noreply.github.com>
2018-05-11 13:13:02 +0200

    Code cleanup
    BUGFIX: DELAY *= 1000
    
    Option menu save effect
    
    Refresh time range feature

--- background.js
+++ background.js
@@ -1,102 +1,36 @@
 /*
-  script that implements searching funtionality
-*/
-
-/*
-  Set the default of times to search, when the person has loaded 
-  the extension for the first time, to 10. Since its done
-  before the onClicked event, the options page will show 10 in the
-  text field
-*/
-var numRefresh = localStorage["times_to_refresh"];
-var appendInt = localStorage["append_int"];
-var inputURL = localStorage["input_url"];
-var DELAY = localStorage["delay"];
-
-if (!numRefresh) {
-  numRefresh = 10;
-  localStorage["times_to_refresh"] = numRefresh;
-}
-
-if (!appendInt) {
-  appendInt = 'true';
-  localStorage["append_int"] = appendInt;
-}
-
-if (!inputURL) {
-  inputURL = "http://www.bing.com/search?q=";
-  localStorage["input_url"] = inputURL;
-}
-
-if(!DELAY){
-  DELAY = 5;
-  localStorage["delay"] = numRefresh;
-}
-
-//console.log("yo");
-
-/*
   What happens when the extension icon is clicked ...
 */
-chrome.browserAction.onClicked.addListener(function(){  
-
-  DELAY *= 1000; //convert to milliseconds
-  /*
-   Get the current tab, tab.id is passed into tab.update so that 
-   only the tab the user is at when he/she presses the button gets 
-   updated. This allows user to move into a different tab and keep
-   working, and also "exit" the extension by closing the tab.
-  */
-  chrome.tabs.getSelected(null, function(tab){
-
-    numRefresh = localStorage["times_to_refresh"];
-    appendInt = localStorage["append_int"];
-    inputURL = localStorage["input_url"];
-
-    if (!numRefresh) {
-      numRefresh = 10;
-      localStorage["times_to_refresh"] = numRefresh;
-    }
-
-    if (!appendInt) {
-      appendInt = 'true';
-      localStorage["append_int"] = appendInt;
-    }
-
-    if (!inputURL) {
-      inputURL = "http://www.bing.com/search?q=";
-      localStorage["input_url"] = inputURL;
-    }
-
-    if(numRefresh < 1) // have to refresh at least once
-      numRefresh = 1;
-
-    var a = 0; 
-    var counter = 0; 
+chrome.browserAction.onClicked.addListener(function() {
+    var numRefresh = localStorage["numRefresh"];
+    var appendInt = localStorage["append_int"];
+    var inputURL = localStorage["input_url"];
+    var DELAY = localStorage["delay"];
+    var PLUS_DELAY = localStorage["plus_delay"];
 
     /*
-      recursive function that searches a random int in bing,
-      and then calls itself with a delay (set at top)
+     Get the current tab, tab.id is passed into tab.update so that 
+     only the tab the user is at when he/she presses the button gets 
+     updated. This allows user to move into a different tab and keep
+     working, and also "exit" the extension by closing the tab.
     */
-    (function search(){
+    chrome.tabs.getSelected(null, function(tab) {
+        var counter = 0;
 
-      if(counter < numRefresh){
+        (function search() {
+            if (numRefresh == 0 || counter < numRefresh) {
+                if (appendInt === 'true') {
+                    var randomInt = Math.floor((Math.random() * 100) + 1);
+                    chrome.tabs.update(tab.id, { 'url': inputURL + randomInt });
+                } else {
+                    chrome.tabs.update(tab.id, { 'url': inputURL });
+                }
 
-        if(appendInt === 'true') {
-          a = Math.floor((Math.random()*100)+1);
-          chrome.tabs.update(tab.id,{'url': inputURL + a});
-        }
+                counter += 1;
 
-        else {chrome.tabs.update(tab.id,{'url': inputURL});}
-
-        counter += 1;               
-        setTimeout(search, DELAY);  
-      }
-      
-    })();
-  });
+                var real_delay = (DELAY + Math.floor((Math.random() * PLUS_DELAY) * 1000));
+                setTimeout(search, real_delay);
+            }
+        })();
+    });
 });
-
-
-
-
--- options.js
+++ options.js
@@ -1,58 +1,44 @@
-// Saves options to localStorage.
 function save_options() {
-  var numRefresh = parseInt(document.getElementById("numRefresh").value);
-  localStorage["times_to_refresh"] = numRefresh;
+    localStorage["numRefresh"] = parseInt(getUiElementByID("numRefresh").value);
+    localStorage["delay"] = parseInt(getUiElementByID("delay").value);
+    localStorage["plus_delay"] = parseInt(getUiElementByID("plus_delay").value);
+    localStorage["append_int"] = getUiElementByID("appendInt").checked;
+    console.log("Checked? " + localStorage["append_int"]);
+    localStorage["input_url"] = getUiElementByID("inputURL").value;
 
-  var delay = parseInt(document.getElementById("delay").value);
-  localStorage["delay"] = delay;
+    var status = getUiElementByID("save");
+    status.value = "Saved!"
+    status.style.background = '#0dd22e';
 
-  var appendInt = document.getElementById("appendInt").checked;
-  localStorage["append_int"] = appendInt;
-
-  var inputURL = document.getElementById("inputURL").value;
-  localStorage["input_url"] = inputURL;
-
-  //chrome.extension.getBackgroundPage().console.log("save:");  
-  //chrome.extension.getBackgroundPage().console.log(appendInt);
-
-  // Update status to let user know options were saved.
-  var status = document.getElementById("save");
-  status.value="Saved!"
-  setTimeout(function() {
-    status.value = "Save";
-  }, 2000);
+    setTimeout(function() {
+        status.value = "Save";
+        status.style.background = '#0298D3';
+    }, 2000);
 }
 
-
-function restore_options() { // populates options from localStorage.
-  var numRefresh = localStorage["times_to_refresh"];
-  var delay = localStorage["delay"];
-  var appendInt = localStorage["append_int"];
-  var inputURL = localStorage["input_url"];
-
-  /*
-  if (!numRefresh || !appendInt || !inputURL || !delay) {
-    return;
-  }
-  */
-  
-  //chrome.extension.getBackgroundPage().console.log("restore:");  
-  //chrome.extension.getBackgroundPage().console.log(appendInt === 'true');
-
-  if(numRefresh)
-    document.getElementById("numRefresh").value = numRefresh;
-  
-  if(delay)
-    document.getElementById("delay").value = delay;
-  
-  if(appendInt)
-    document.getElementById("appendInt").checked = (appendInt === 'true');
-  
-  if(inputURL)
-    document.getElementById("inputURL").value = inputURL;
-
+function getUiElementByID(elementName) {
+    return document.getElementById(elementName);
 }
 
-// Event Listners
+function restore_options() {
+    if (isNaN(parseInt(localStorage["delay"]))) {
+        inicializeVariables();
+    } else {
+        document.getElementById("numRefresh").value = localStorage["numRefresh"];
+        document.getElementById("delay").value = localStorage["delay"];
+        document.getElementById("plus_delay").value = localStorage["plus_delay"];
+        document.getElementById("appendInt").checked = (localStorage["append_int"] === 'true');
+        document.getElementById("inputURL").value = localStorage["input_url"];
+    }
+}
+
+function inicializeVariables() {
+    localStorage["numRefresh"] = 0;
+    localStorage["delay"] = 10;
+    localStorage["plus_delay"] = 2;
+    localStorage["append_int"] = false;
+    localStorage["input_url"] = "https://www.google.com/";
+}
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#save').addEventListener('click', save_options);
